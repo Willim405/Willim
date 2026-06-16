f177a9d5bcbf7f0792084c78839f4c1cb92af341
Willim405 <almobshergames1@gmail.com>
2026-06-16 12:35:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,20 +3,19 @@ import { group, access } from "./system/control.js";
 import UltraDB from "./system/UltraDB.js";
 import sub from './sub.js';
 
+/* ╔════════════════════════════════════════════════════════════════════════════╗
+⚜️ 𝑾𝑰𝑳𝑳𝑰𝑨𝑴 ⚜️ 𝑴𝑶𝑹𝑰𝑨𝑹𝑻𝒀 ⚜️
+   ☠️ Master of Crime – Moriarty
+   ╚════════════════════════════════════════════════════════════════════════════╝ */
+
 /* =========== Client ========== */
 const client = new Client({
-  phoneNumber: '20123456789', // Bot number
+  phoneNumber: '201038694952', // رقم البوت
   prefix: [".", "/", "!"],
   fromMe: false, 
   owners: [
-  // Owner 1
-    { name: "VA", lid: "247579682029763@lid", jid: "972569311531@s.whatsapp.net" },
-  // Owner 2
-    { name: "emam", lid: "221307316789354@lid", jid: "201144480436@s.whatsapp.net" },
-  // Owner 3
-    { name: "Sukuna", jid: "201033024135@s.whatsapp.net", lid: "50414477168824@lid" },
-  // Owner 4 
-   { name: "عمورتي", jid: "201050079089@s.whatsapp.net", lid: "51664513925368@lid" }
+  // ★ المطور الرئيسي ★
+    { name: "𝑾𝑰𝑳𝑳𝑰𝑨𝑴", jid: "201038694952@s.whatsapp.net", lid: "201038694952@lid" }
   ],
   settings: { noWelcome: false },
   commandsPath: './plugins'
@@ -33,17 +32,17 @@ if (!global.db) {
 /* =========== Config ========== */
 const { config } = client;
 config.info = { 
-  nameBot: "♡ 𝙋𝙊𝙈𝙉𝙄 🎪 〈", 
-  nameChannel: "𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️", 
+  nameBot: "𝑾𝑰𝑳𝑳𝑰𝑨𝑴",
+  nameChannel: "𝑾𝑰𝑳𝑳𝑰𝑨𝑴", 
   idChannel: "120363225356834044@newsletter",
   urls: {
-    repo: "https://github.com/deveni0/Pomni-AI",
-    api: "https://emam-api.web.id",
-    channel: "https://whatsapp.com/channel/0029VaQim2bAu3aPsRVaDq3v"
+    repo: "https://github.com/Willim/Willim-Bot",
+    api: "https://willim-api.web.id",
+    channel: "https://whatsapp.com/channel/0029VbDakGAEKyZBVXYq7w3e"
   },
   copyright: { 
-    pack: 'ڤـ ـ VA ـ ـا', 
-    author: 'VA'
+    pack: '⚜️ 𝑾𝑰𝑳𝑳𝑰𝑨𝑴 ⚜️', 
+    author: '𝑴𝑶𝑹𝑰𝑨𝑹𝑻𝒀'
   },
   images: [
     "https://i.pinimg.com/originals/11/26/97/11269786cdb625c60213212aa66273a9.png",
