a68bcea1678036b375f70130a6316304dca490bd
Willim405 <almobshergames1@gmail.com>
2026-06-18 20:49:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -49,12 +49,13 @@ if (fs.existsSync(mediaPath)) {
 
 // لو مفيش صور، استخدم الروابط القديمة
 if (images.length === 0) {
-    images = [
-        "https://i.pinimg.com/originals/11/26/97/11269786cdb625c60213212aa66273a9.png",
-        "https://i.pinimg.com/originals/e2/21/20/e221203f319df949ee65585a657501a2.jpg",
-        "https://i.pinimg.com/originals/bb/77/0f/bb770fad66a634a6b3bf93e9c00bf4e5.jpg"
-    ];
-}
+     images = [
+    path.join(__dirname, 'media', '1m.jpg'),
+    path.join(__dirname, 'media', '2m.jpg'),
+    path.join(__dirname, 'media', '3m.jpg'),
+    path.join(__dirname, 'media', '4m.jpg'),
+    path.join(__dirname, 'media', '5m.jpg')
+];
 
 /* =========== Config ========== */
 const { config } = client;
@@ -63,7 +64,7 @@ config.info = {
   nameChannel: "🩸 𝑾𝑰𝑳𝑳𝑰𝑨𝑴 𝑩𝑶𝑻 🕊",
   idChannel: "120363437236619633@newsletter",
   urls: {
-    repo: "https://github.com/Willim/Willim-Bot",
+    repo: "https://github.com/Willim405/Willim.git",
     api: "https://willim-api.web.id",
     channel: "https://whatsapp.com/channel/0029VbDakGAEKyZBVXYq7w3e"
   },
@@ -90,52 +91,4 @@ process.on('uncaughtException', (e) => {
 
 process.on('unhandledRejection', (err) => {
     console.error('Unhandled Rejection:', err)
-});  idChannel: "120363225356834044@newsletter",
-  urls: {
-    repo: "https://github.com/Willim/Willim-Bot",
-    api: "https://willim-api.web.id",
-    channel: "https://whatsapp.com/channel/0029VbDakGAEKyZBVXYq7w3e"
-  },
-  copyright: { 
-    pack: '⚜️ 𝑾𝑰𝑳𝑳𝑰𝑨𝑴 ⚜️', 
-    author: '𝑴𝑶𝑹𝑰𝑨𝑹𝑻𝒀'
-  },
-  images: [
-    "https://i.pinimg.com/originals/11/26/97/11269786cdb625c60213212aa66273a9.png",
-    "https://i.pinimg.com/originals/e2/21/20/e221203f319df949ee65585a657501a2.jpg",
-    "https://i.pinimg.com/originals/bb/77/0f/bb770fad66a634a6b3bf93e9c00bf4e5.jpg"
-  ]
-};
-
-/* =========== Start ========== */
-client.start();
-
-setTimeout(async () => {
-if (client.commandSystem) { 
-sub(client)
-  }
-}, 2000);
-
-
-/* =========== Catch Errors ========== */
-process.on('uncaughtException', (e) => {
-    if (e.message.includes('rate-overlimit')) {}
 });
-
-process.on('unhandledRejection', (err) => {
-    console.error('Unhandled Rejection:', err)
-});
-
-
-/* 
-=========== Memory Monitor ========== 
-
-setInterval(() => {
-    const used = process.memoryUsage().rss / 1024 / 1024
-    if (used > 800) {
-        console.log(`🔄 Bot memory full (${used.toFixed(1)}MB), restarting...`)
-        process.exit(1) 
-    }
-}, 300_000) 
-
-*/
